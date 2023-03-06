8b2dd5dba602536fc2580d33f3fdee39c1a5afdc
fabibert <76185888+fabibert@users.noreply.github.com>
2023-03-06 19:01:22 +0100

    remove welcome text

--- src/components/views/Landing.js
+++ src/components/views/Landing.js
@@ -19,28 +19,19 @@ const Landing = props => {
         <BaseContainer>
             <div className="landing container">
                 <div className="landing form">
-                    <div className="landing align vertically">
-                        <div className="landing align horizontal">
-                            <div className="landing welcome-text">
-                                Welcome
-                            </div>
-                            <div className="landing button-holder">
-                                <div className="landing button-container">
-                                    <Button
-                                        onClick={handleLoginClick}
-                                    >
-                                        Login
-                                    </Button>
-                                </div>
-                                <div className="landing button-container">
-                                    <Button
-                                        onClick={handleRegisterClick}
-                                    >
-                                        Register
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="landing button-container">
+                        <Button
+                            onClick={handleLoginClick}
+                        >
+                            Login
+                        </Button>
+                    </div>
+                    <div className="landing button-container">
+                        <Button
+                            onClick={handleRegisterClick}
+                        >
+                            Register
+                        </Button>
                     </div>
                 </div>
             </div>
