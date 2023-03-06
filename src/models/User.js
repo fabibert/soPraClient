2d1eabcd0d9a4f238ce2fbd5746f7acbef19bd54
fabibert <76185888+fabibert@users.noreply.github.com>
2023-03-06 17:50:41 +0100

    adjust user attributes to include birthday

--- src/models/User.js
+++ src/models/User.js
@@ -14,6 +14,7 @@ class User {
         //TODO: unused password and creationDate
         this.password = null;
         this.creationDate = null;
+        this.birthDay = null;
         Object.assign(this, data);
     }
 }
